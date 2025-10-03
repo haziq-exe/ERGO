c4a13df86b82b0592d54f915459988bca9d2009c
haziq-exe <haziqmk123@hotmail.com>
2025-10-04 01:18:57 +0400

    fix

--- core/model.py
+++ core/model.py
@@ -180,7 +180,7 @@ class LocalLLMModel(BaseModel):
             else:
                 avg_entropy = None
 
-        output_length = outputs.shape[1]
+        num_new_tokens = outputs.sequences.shape[1] - input_length
         response_ids = outputs.sequences
         input_len = inputs["input_ids"].shape[1]
         new_tokens = response_ids[:, input_len:].cpu()
@@ -192,7 +192,7 @@ class LocalLLMModel(BaseModel):
         r0 = self.generate_with_temperature(messages, temperature=0.2)
         r1 = re.sub(r"<think>[\s\S]*?(?:</think>|$)", "", response_only, flags=re.DOTALL)
         # perturb: 1% of length of r1, at least 1 token
-        perturb_n = max(1, int(0.01 * (output_length - input_length)))
+        perturb_n = max(1, int(0.01 * num_new_tokens))
         rh = self.generate_with_temperature(messages, temperature=1.0, perturb_first_n=perturb_n)
 
         r0 = re.sub(r"<think>[\s\S]*?(?:</think>|$)", "", r0, flags=re.DOTALL)
